3f143e70e89300a4f6e945ecc17431cff182869c
iamLalitha <lalithasans10@gmail.com>
2023-05-19 14:44:54 +0530

    title change

---
+++ JS-files/map.js
@@ -0,0 +1,5 @@
+let numbers=[1,2,3,4,5];
+function incrementBy5(value,index,array){
+    return value + 5;
+}
+console.log(numbers.map(incrementBy5));
---
+++ JS-files/paractice.js
@@ -0,0 +1,8 @@
+let N=[1,2,3,3];
+let K= 2;
+if(K in N){
+    console.log('yes')
+}
+else{
+    console.log('No')
+}
